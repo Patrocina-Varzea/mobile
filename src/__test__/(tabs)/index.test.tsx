3a68bd29c36af8c34f0b75211a9bdd8f583f2574
Raphael Renato <raphaelrenato.medeirosdematta-ext@ab-inbev.com>
2024-05-13 15:28:47 -0300

    chore: add jest and testing library

---
+++ src/__test__/(tabs)/index.test.tsx
@@ -0,0 +1,15 @@
+import Home from "@/app/(tabs)";
+import { render } from "@testing-library/react-native";
+
+describe("Home", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render text and button correctly", () => {
+    const { getByTestId } = render(<Home />);
+
+    expect(getByTestId("label-home")).toBeDefined();
+    expect(getByTestId("button-label-home")).toBeDefined();
+  });
+});
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,9 +3,11 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl text-gray-900 mb-8">Home</Text>
-      <Button label="Home" />
+    <View className="flex-1 items-center justify-center">
+      <Text className="mb-8 text-3xl text-gray-900" testID="label-home">
+        Home
+      </Text>
+      <Button label="Home" testID="button-label-home" />
     </View>
   );
 }
